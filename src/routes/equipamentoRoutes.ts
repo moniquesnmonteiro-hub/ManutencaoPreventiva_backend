1eeec20ab6e4b3a8abb9295339840c9f1bf043e7
monique <moniquesnmonteiro@gmail.com>
2026-04-28 21:51:51 -0400

    fix: validacao de perfil para gerenciar Equipamentos

--- src/routes/equipamentoRoutes.ts
+++ src/routes/equipamentoRoutes.ts
@@ -3,6 +3,8 @@ import { EquipamentoService } from "../services/EquipamentoService.js";
 import EquipamentoController from "../controllers/EquipamentoController.js";
 import { validateBody } from "../middleware/validateBody.js";
 import { createEquipamentoSchema, updateEquipamentoSchema } from "../dtos/CreateEquipamentoSchema.js";
+import { ensureRole } from "../middleware/ensureRole.js";
+import { Perfil } from "../types/Perfil.js";
 
 const router = Router();
 const equipamentoService = new EquipamentoService();
@@ -14,16 +16,22 @@ router.get("/:id", equipamentoController.findOne.bind(equipamentoController));
 
 router.post(
     "/", 
+    ensureRole(Perfil.SUPERVISOR, Perfil.GESTOR),
     validateBody(createEquipamentoSchema), 
     equipamentoController.create.bind(equipamentoController)
 );
 
 router.put(
     "/:id", 
+    ensureRole(Perfil.SUPERVISOR, Perfil.GESTOR),
     validateBody(updateEquipamentoSchema), 
     equipamentoController.update.bind(equipamentoController)
 );
 
-router.delete("/:id", equipamentoController.delete.bind(equipamentoController));
+router.delete(
+    "/:id", 
+    ensureRole(Perfil.GESTOR), 
+    equipamentoController.delete.bind(equipamentoController)
+);
 
 export default router;
